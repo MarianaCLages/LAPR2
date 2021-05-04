81587ca1dd15ad2ad7f7361eeb507d1073e04279
Eduardo Novo <1200737@isep.ipp.pt>
2021-05-04 15:25:34 +0100

    US10 - Tests check

--- src/main/java/app/domain/model/Parameter.java
+++ src/main/java/app/domain/model/Parameter.java
@@ -71,39 +71,10 @@ public class Parameter {
 
     @Override
     public String toString() {
-        return "Parameter{" +
-                "code='" + code + '\'' +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", category='" + cat.toString() + '\'' +
-                '}';
-    }
-
-    /**
-     * Returns the code of the Parameter
-     *
-     * @return code: unique code needed to identify the Parameter
-     */
-
-    public String getCode() {
-        return code;
-    }
-
-    /**
-     * Returns the name of the Parameter
-     *
-     * @return name: unique name needed to identify the Parameter
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * modifies the code of the Parameter
-     *
-     * @param name unique name needed to identify the Parameter
-     */
-    public void setName(String name) {
-        this.name = name;
+        return "Parameter: " +
+                "code=" + code +
+                ", name=" + name +
+                ", description=" + description +
+                ", category=" + cat.toString();
     }
 }
