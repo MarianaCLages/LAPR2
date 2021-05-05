734199b0a305aad369ed81e81f924daecddf05c1
Eduardo Sousa <1200920@isep.ipp.pt>
2021-05-05 21:38:07 +0100

    Revert "Add constant Role_Client"
    This reverts commit da6b01373577f9fca589e628339d2809c7d8d0aa.

--- src/main/java/app/domain/shared/Constants.java
+++ src/main/java/app/domain/shared/Constants.java
@@ -1,11 +1,11 @@
 package app.domain.shared;
 
 /**
+ *
  * @author Paulo Maio <pam@isep.ipp.pt>
  */
 public class Constants {
     public static final String ROLE_ADMIN = "ADMINISTRATOR";
-    public static final String ROLE_CLIENT = "CLIENT";
 
 
     public static final String PARAMS_FILENAME = "config.properties";
