98df49bf7d5582d3a32ee40bcd337d73057ea683
Eduardo Sousa <1200920@isep.ipp.pt>
2021-05-06 20:52:39 +0100

    Company Tests

--- src/test/java/app/domain/model/CompanyTest.java
+++ src/test/java/app/domain/model/CompanyTest.java
@@ -2,21 +2,42 @@ package app.domain.model;
 
 import app.domain.shared.Constants;
 import auth.AuthFacade;
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class CompanyTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void GetDesignationBlank(){
+    public void GetDesignationBlank() {
         Company company = new Company("");
     }
 
     @Test
-    public void getAuthFacadeTest(){
+    public void getAuthFacadeTest() {
         Company company = new Company(Constants.PARAMS_COMPANY_DESIGNATION);
         AuthFacade auth = company.getAuthFacade();
+    }
 
+
+    @Test
+    public void getDesignationTest() {
+        Company company = new Company(Constants.PARAMS_COMPANY_DESIGNATION);
+        String actual = company.getDesignation();
+        String expected = Constants.PARAMS_COMPANY_DESIGNATION;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void getParameterCategoryList() {
+        Company company = new Company(Constants.PARAMS_COMPANY_DESIGNATION);
+        ParameterCategoryStore store = company.getParameterCategoryList();
+    }
+
+    @Test
+    public void getClientList() {
+        Company company = new Company(Constants.PARAMS_COMPANY_DESIGNATION);
+        ClientStore store = company.getClientList();
     }
 }
