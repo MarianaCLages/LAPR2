5b752bd52853c3dc8bcd94a3542c2f750a8a3779
Eduardo Sousa <1200920@isep.ipp.pt>
2021-05-06 19:20:34 +0100

    Tests Controller

--- src/test/java/app/controller/ParameterCategoryControllerTest.java
+++ src/test/java/app/controller/ParameterCategoryControllerTest.java
@@ -1,4 +1,51 @@
+package app.controller;
+
+import app.domain.model.Company;
+import app.domain.model.ParameterCategory;
+import app.domain.model.ParameterCategoryStore;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class ParameterCategoryControllerTest {
-  
+    Company company =App.getInstance().getCompany();
+
+    @Test
+    public void CreateController(){
+        ParameterCategoryController controller = new ParameterCategoryController(company);
+    }
+
+
+    @Test
+    public void CreateParameterCategory(){
+        ParameterCategoryController controller = new ParameterCategoryController(company);
+
+        ParameterCategoryStore store = company.getParameterCategoryList();
+        store.CreateParameterCategory("AF164","Hemogram");
+    }
+
+    @Test
+    public void GetParameterCategory(){
+        ParameterCategoryController controller = new ParameterCategoryController(company);
+
+        controller.createParameterCategory("AF164","Hemogram");
+
+        String expected = "Code = AF164 Name=Hemogram";
+        String actual= controller.getpc();
+
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void saveParameterCategory(){
+        ParameterCategoryController controller = new ParameterCategoryController(company);
+
+        controller.createParameterCategory("AF164","Hemogram");
+
+        Assert.assertTrue(controller.saveParameterCategory());
+    }
+
+
+
 }
