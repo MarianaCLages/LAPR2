bb68ef29539171933258a058be41aff14d418ef6
Eduardo Novo <1200737@isep.ipp.pt>
2021-05-06 17:03:49 +0100

    US7 - Employee tests

--- src/test/java/app/domain/model/EmployeeTest.java
+++ src/test/java/app/domain/model/EmployeeTest.java
@@ -7,22 +7,65 @@ import static org.junit.Assert.*;
 public class EmployeeTest {
     Role role = new Role("1", "Medical Lab Technician" );
 
+    @Test
+    public void RegisterEmployee() {
+        //Arrange + Act
+        Employee employee = new Employee("Bino","AtuaTerra", "912345678","something@isep.com","111111111111111111",role);
+    }
+
+
     @Test(expected = IllegalArgumentException.class)
     public void RegisterEmployeeNameWithNum() {
         //Arrange + Act
-        Employee employee = new Employee("Bino1","AtuaTerra", "912345678","something@isep.com","11111111111111111",role  );
+        Employee employee = new Employee("Bino1","AtuaTerra", "912345678","something@isep.com","11111111111111111",role);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void CreateEmployeeNameBlank() {
+        //Arrange + Act
+        Employee employee = new Employee("","AtuaTerra", "912345678","something@isep.com","11111111111111111",role);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void CreateEmployeeAddressBlank() {
+        //Arrange + Act
+        Employee employee = new Employee("Bino","", "912345678","something@isep.com","11111111111111111",role);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void RegisterEmployeePhoneNumberWithLetter() {
         //Arrange + Act
-        Employee employee = new Employee("Bino","AtuaTerra", "91234567A","something@isep.com","11111111111111111",role  );
+        Employee employee = new Employee("Bino","AtuaTerra", "91234567A","something@isep.com","11111111111111111",role);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void CreateEmployeePhoneNumberBlank() {
+        //Arrange + Act
+        Employee employee = new Employee("Bino","AtuaTerra", "","something@isep.com","11111111111111111",role);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void CreateEmployeeEmailBlank() {
+        //Arrange + Act
+        Employee employee = new Employee("Bino","AtuaTerra", "912345678","","11111111111111111",role);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void CreateEmployeeEmailWrongFormat() {
+        //Arrange + Act
+        Employee employee = new Employee("Bino","AtuaTerra", "912345678","bino@fusivel","11111111111111111",role);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void CreateEmployeeSOCBlank() {
+        //Arrange + Act
+        Employee employee = new Employee("Bino","AtuaTerra", "912345678","","",role);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void RegisterEmployeeSOCWithLetter() {
         //Arrange + Act
-        Employee employee = new Employee("Bino","AtuaTerra", "912345678","something@isep.com","11111111111111111A",role  );
+        Employee employee = new Employee("Bino","AtuaTerra", "912345678","something@isep.com","11111111111111111A",role);
     }
 
 }
