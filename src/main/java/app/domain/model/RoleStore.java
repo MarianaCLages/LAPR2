206b9e97256bdfdd20a6ab50528861a63a5dbf4c
Eduardo Sousa <1200920@isep.ipp.pt>
2021-05-09 01:30:26 +0100

    test the controllers

--- src/main/java/app/domain/model/RoleStore.java
+++ src/main/java/app/domain/model/RoleStore.java
@@ -1,26 +1,26 @@
-
 package app.domain.model;
 
-import javax.management.relation.RoleList;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Class that represent the list of all the roles of the system
+ */
 public class RoleStore {
 
 
-
-    private  List <Role> array;
-    private Role pc;
-
-    private final Role ClinicalChemistryTechnologist = new Role("0","Clinical Chemistry Technologist");
+    private final Role ClinicalChemistryTechnologist = new Role("0", "Clinical Chemistry Technologist");
     private final Role MedicalLabTechnician = new Role("1", "Medical Lab Technician");
     private final Role LaboratoryCoordinator = new Role("2", "LaboratoryCoordinator");
-    private final Role Receptionist = new Role("3","Receptionist");
-    private final Role SpecialistDoctor = new Role("4","SpecialistDoctor");
+    private final Role Receptionist = new Role("3", "Receptionist");
+    private final Role SpecialistDoctor = new Role("4", "SpecialistDoctor");
+    private List<Role> array;
+    private Role pc;
 
-
-    public RoleStore(){
+    /**
+     * Constructor of the class it creates an empty list and fills it with the roles of the Company
+     */
+    public RoleStore() {
         this.array = new ArrayList<>();
 
         array.add(ClinicalChemistryTechnologist);
@@ -31,29 +31,24 @@ public class RoleStore {
     }
 
 
-
-
-
-
-    public Role get(int index ) {
+    /**
+     * This method search for an Role object by the index of that object in the ArrayList
+     *
+     * @param index index of the array list where we want to get the object
+     * @return the Role object that was in the index of the array list
+     */
+    public Role get(int index) {
         return array.get(index);
     }
 
 
-
-
-
-    public List<Role> getRoleList(){
+    /**
+     * @return the list of Roles
+     */
+    public List<Role> getRoleList() {
 
         return this.array;
     }
-
-
-
-
-
-
-
 
 
 }
