b4ed3957996b251ecfb2ae294911ba50fbbdbcc6
Eduardo Sousa <1200920@isep.ipp.pt>
2021-05-25 20:12:03 +0100

    debug commit

--- src/main/java/app/domain/model/Sample.java
+++ src/main/java/app/domain/model/Sample.java
@@ -1,15 +1,15 @@
 package app.domain.model;
 
-import app.domain.stores.TestParameterStore;
+
 import net.sourceforge.barbecue.Barcode;
 
 public class Sample {
 
     private String collectingMethod;
-    private net.sourceforge.barbecue.Barcode barcode;
+    private Barcode barcode;
 
 
-    public Sample(net.sourceforge.barbecue.Barcode barcode){
+    public Sample(Barcode barcode){
         this.barcode = barcode;
     }
 
