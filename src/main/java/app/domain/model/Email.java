24a58832680ddcf2f7ee286954909a5a2af02286
Eduardo Sousa <1200920@isep.ipp.pt>
2021-05-27 21:22:34 +0100

    Corrections on diagrams of US12

--- src/main/java/app/domain/model/Email.java
+++ src/main/java/app/domain/model/Email.java
@@ -5,6 +5,7 @@ import app.domain.shared.Constants;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Class that represents the email that should be sent to the user when he is registered.
@@ -13,14 +14,21 @@ import java.io.IOException;
 public class Email {
     public static void sendPasswordNotification(String name, String email, String password) {
         File passFile = new File(Constants.FILE);
-
+        FileWriter writer = null;
         try {
-            FileWriter writer = new FileWriter(passFile);
-            writer.write("MR/MS "+ name +" you were registered with the email "+ email+ " and your password is "+password);
+            writer = new FileWriter(passFile);
+            writer.write("MR/MS " + name + " you were registered with the email " + email + " and your password is " + password);
             writer.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
-        }
-    }
+        } finally {
+            try {
+                Objects.requireNonNull(writer).close();
 
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
 }
