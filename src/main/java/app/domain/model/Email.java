18ec73701ae307ed4108d3ceea408706328d01ec
Eduardo Sousa <1200920@isep.ipp.pt>
2021-05-28 11:12:17 +0100

    try-with-resources in the Email.java class

--- src/main/java/app/domain/model/Email.java
+++ src/main/java/app/domain/model/Email.java
@@ -5,7 +5,6 @@ import app.domain.shared.Constants;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Objects;
 
 /**
  * Class that represents the email that should be sent to the user when he is registered.
@@ -14,20 +13,10 @@ import java.util.Objects;
 public class Email {
     public static void sendPasswordNotification(String name, String email, String password) {
         File passFile = new File(Constants.FILE);
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter(passFile);
+        try (FileWriter writer = new FileWriter(passFile)) {
             writer.write("MR/MS " + name + " you were registered with the email " + email + " and your password is " + password);
-            writer.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                Objects.requireNonNull(writer).close();
-
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
         }
 
     }
