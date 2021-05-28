394e2495d55536fc4b35ce1d118a2b4af02cf1a7
Eduardo Novo <1200737@isep.ipp.pt>
2021-05-28 02:15:16 +0100

    US14 - Test Testes

--- src/test/java/app/domain/model/TestTest.java
+++ src/test/java/app/domain/model/TestTest.java
@@ -220,4 +220,79 @@ public class TestTest {
         Assert.assertFalse(value);
     }
 
+    @Test
+    public void createReportTest(){
+        ParameterCategoryStore cat = new ParameterCategoryStore();
+        ParameterCategory pc1 = new ParameterCategory("AH000", "Hemogram");
+        cat.add(pc1);
+        List<ParameterCategory> cat1 = new ArrayList<>();
+        cat1.add(pc1);
+        List<Parameter> pa = new ArrayList<>();
+        Parameter p1 = new Parameter("AH000","Nome","description",pc1);
+        pa.add(p1);
+        TestType testType = new TestType("BL000","description","sei lá",cat);
+
+        app.domain.model.Test test = new app.domain.model.Test("1234s","123456789012","1234567890123456",testType,cat1,pa);
+        boolean rep = test.createReport("a");
+        Assert.assertTrue(rep);
+    }
+
+    @Test
+    public void saveReportTest(){
+        ParameterCategoryStore cat = new ParameterCategoryStore();
+        ParameterCategory pc1 = new ParameterCategory("AH000", "Hemogram");
+        cat.add(pc1);
+        List<ParameterCategory> cat1 = new ArrayList<>();
+        cat1.add(pc1);
+        List<Parameter> pa = new ArrayList<>();
+        Parameter p1 = new Parameter("AH000","Nome","description",pc1);
+        pa.add(p1);
+        TestType testType = new TestType("BL000","description","sei lá",cat);
+
+        app.domain.model.Test test = new app.domain.model.Test("1234s","123456789012","1234567890123456",testType,cat1,pa);
+
+        test.saveReport();
+        String actual = test.getState();
+        String expected = "DIAGNOSTIC_MADE";
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void getIDTest(){
+        ParameterCategoryStore cat = new ParameterCategoryStore();
+        ParameterCategory pc1 = new ParameterCategory("AH000", "Hemogram");
+        cat.add(pc1);
+        List<ParameterCategory> cat1 = new ArrayList<>();
+        cat1.add(pc1);
+        List<Parameter> pa = new ArrayList<>();
+        Parameter p1 = new Parameter("AH000","Nome","description",pc1);
+        pa.add(p1);
+        TestType testType = new TestType("BL000","description","sei lá",cat);
+
+        app.domain.model.Test test = new app.domain.model.Test("1234s","123456789012","1234567890123456",testType,cat1,pa);
+
+        String actual = test.getID();
+        String expected = "1234s";
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void getStateTest(){
+        ParameterCategoryStore cat = new ParameterCategoryStore();
+        ParameterCategory pc1 = new ParameterCategory("AH000", "Hemogram");
+        cat.add(pc1);
+        List<ParameterCategory> cat1 = new ArrayList<>();
+        cat1.add(pc1);
+        List<Parameter> pa = new ArrayList<>();
+        Parameter p1 = new Parameter("AH000","Nome","description",pc1);
+        pa.add(p1);
+        TestType testType = new TestType("BL000","description","sei lá",cat);
+
+        app.domain.model.Test test = new app.domain.model.Test("1234s","123456789012","1234567890123456",testType,cat1,pa);
+
+        test.saveReport();
+
+        Assert.assertEquals(test.getState(),"DIAGNOSTIC_MADE");
+    }
+
 }
