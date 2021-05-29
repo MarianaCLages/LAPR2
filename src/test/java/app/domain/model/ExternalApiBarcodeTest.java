631b1497a4e6e1c42bc20e7eb513d8e1f08468e7
1200737 <1200737@isep.ipp.pt>
2021-05-29 17:29:00 +0100

    ExternalApiBarcode Tests

--- src/test/java/app/domain/model/ExternalApiBarcodeTest.java
+++ src/test/java/app/domain/model/ExternalApiBarcodeTest.java
@@ -1,8 +1,17 @@
 package app.domain.model;
 
+import app.domain.stores.SampleStore;
+import net.sourceforge.barbecue.Barcode;
 import net.sourceforge.barbecue.BarcodeException;
+import net.sourceforge.barbecue.BarcodeFactory;
+import net.sourceforge.barbecue.Module;
+import net.sourceforge.barbecue.output.Output;
+import net.sourceforge.barbecue.output.OutputException;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class ExternalApiBarcodeTest {
 
@@ -17,4 +26,98 @@ public class ExternalApiBarcodeTest {
         Assert.assertEquals(expected, actual);
 
     }
+
+    @Test
+    public void createBarcodeImageTest() throws BarcodeException, OutputException, IOException {
+        SampleStore store = new SampleStore();
+        Sample rep = new Sample("123456789012", "12345678901");
+
+        store.saveSample();
+        BarcodeAdapter em = new BarcodeAdapter() {
+            @Override
+            public String createBarcode(String ID) throws BarcodeException {
+                return null;
+            }
+
+            @Override
+            public void createBarcodeImage(Barcode barcode) throws OutputException {
+
+            }
+
+            @Override
+            public String getBarcode() {
+                return null;
+            }
+
+            @Override
+            public void barcodeImage() throws IOException, OutputException {
+
+            }
+        };
+        em.barcodeImage();
+    }
+
+    @Test
+    public void getBarcodeTest() throws IOException, OutputException {
+        //Arrange + Act
+        SampleStore store = new SampleStore();
+        Sample rep = new Sample("123456789012", "12345678901");
+
+        store.saveSample();
+        BarcodeAdapter em = new BarcodeAdapter() {
+            @Override
+            public String createBarcode(String ID) throws BarcodeException {
+                return null;
+            }
+
+            @Override
+            public void createBarcodeImage(Barcode barcode) throws OutputException {
+
+            }
+
+            @Override
+            public String getBarcode() {
+                return null;
+            }
+
+            @Override
+            public void barcodeImage() throws IOException, OutputException {
+
+            }
+        };
+        em.getBarcode();
+
+
+    }
+
+    @Test
+    public void barcodeImageTest() throws IOException, OutputException {
+        SampleStore store = new SampleStore();
+        Sample rep = new Sample("123456789012", "12345678901");
+
+        store.saveSample();
+        BarcodeAdapter em = new BarcodeAdapter() {
+            @Override
+            public String createBarcode(String ID) throws BarcodeException {
+                return null;
+            }
+
+            @Override
+            public void createBarcodeImage(Barcode barcode) throws OutputException {
+
+            }
+
+            @Override
+            public String getBarcode() {
+                return null;
+            }
+
+            @Override
+            public void barcodeImage() throws IOException, OutputException {
+
+            }
+        };
+        em.barcodeImage();
+    }
+
 }
