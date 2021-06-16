e5c8713280adf95ba2500986820481db12060122
Eduardo Sousa <1200920@isep.ipp.pt>
2021-06-16 10:10:06 +0100

    MaxSumAdapter

---
+++ src/main/java/app/domain/model/MaxSumAdapterBruteForce.java
@@ -0,0 +1,8 @@
+package app.domain.model;
+
+public class MaxSumAdapterBruteForce implements MaxSumAdapter {
+    @Override
+    public int[] getMaxSum(int[] array) {
+        return new int[0];
+    }
+}
