2553f9e47ac2ceb67f6756c13cc6a3fcef30cae4
1200300 <1200300@isep.ipp.pt>
2021-06-17 15:47:45 +0100

    Implementation in progress for UI US2

--- src/main/java/app/ui/console/ClientUpdateUI.java
+++ src/main/java/app/ui/console/ClientUpdateUI.java
@@ -25,7 +25,30 @@ public class ClientUpdateUI implements Runnable {
         sexes.add("Female");
         sexes.add("Other");
 
-       
+        do {
+            try {
+                String cc = Utils.readLineFromConsole("Please enter the new cc");
+                String name = Utils.readLineFromConsole("Please enter the new name");
+                String nhs = Utils.readLineFromConsole("Please enter the new nhs");
+                Date birthDate = Utils.readDateFromConsole("Please enter the new birth date");
+                int sexIndex = Utils.showAndSelectIndex(sexes, "Please choose your updated sex");
+                char sex = ' ';
+                if (sexIndex == 0) {
+                    sex = 'M';
+                } else if (sexIndex == 1) {
+                    sex = 'S';
+                }
+                String tif = Utils.readLineFromConsole("Please enter the new TIF number");
+                String phoneNumber = Utils.readLineFromConsole("Please enter the new phone number");
+                String email = Utils.readLineFromConsole("Please enter the email of the new Client");
+                clientDataController.updateClientData(cc, nhs, birthDate, sex, tif, phoneNumber, email, name);
+                exception = false;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("An error occurred during the creation during the creation of the Parameter please try again");
+                exception = true;
+            }
+        } while (exception);
 
 
     }
