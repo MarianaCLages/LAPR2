5383188d3bcc8babb464fa2be99ffa4d095ff8d4
AzureAD\MiguelJordão(1201487 <1201487@isep.ipp.pt>
2021-06-18 15:19:28 +0100

    Update in US018

--- src/main/java/app/ui/gui/adminMenuUIs/MultiLinearRegressionUI.java
+++ src/main/java/app/ui/gui/adminMenuUIs/MultiLinearRegressionUI.java
@@ -1,16 +1,31 @@
 package app.ui.gui.adminMenuUIs;
 
 import app.controller.App;
+import app.controller.GenerateNHSReportController;
 import app.domain.shared.Constants;
 import app.controller.SceneController;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
 public class MultiLinearRegressionUI {
 
+    @FXML
+    private TextArea myTextAreaMulti;
+    @FXML
+    private Button myReturnButtonMulti;
+
     private SceneController sceneController= SceneController.getInstance();
+    private App app = sceneController.getApp();
+    private GenerateNHSReportController ctrl = SceneController.getInstance().getCtrl();
+
+    public MultiLinearRegressionUI(){
+       ctrl.multiRegression();
+      // myTextAreaMulti.setText(ctrl.getSb().toString());
+    }
 
     public void returnToGenerateNHSReport(ActionEvent event) {
-        App app = sceneController.getApp();
         app.doLogout();
         sceneController.switchMenu(event, Constants.NHS_REPORT_UI);
     }
