d66e768e25a81713a68d034d21016efbcd427956
1200902 Mariana Lages <1200902@isep.ipp.pt>
2021-06-19 23:15:21 +0100

    Javadoc (US18 + US19)

--- src/main/java/app/controller/SceneController.java
+++ src/main/java/app/controller/SceneController.java
@@ -20,13 +20,23 @@ public class SceneController {
     private App app = App.getInstance();
     private Company company = app.getCompany();
 
+    private final CheckPerformanceController ctrl = new CheckPerformanceController();
+    private Runnable ui;
+
+    /**
+     * Gets the Check Performance Controller.
+     *
+     * @return the Check Performance Controller
+     */
     public CheckPerformanceController getCtrl() {
         return ctrl;
     }
 
-    private final CheckPerformanceController ctrl = new CheckPerformanceController();
-    private Runnable ui;
-
+    /**
+     * Gets the instance.
+     *
+     * @return the singleton (Scene Controller)
+     */
     public static SceneController getInstance() {
 
         if (singleton == null) {
@@ -37,18 +47,33 @@ public class SceneController {
         return singleton;
     }
 
+    /**
+     * Gets the auth controller.
+     *
+     * @return the auth controller
+     */
     public AuthController getAuthController() {
         return authController;
     }
 
+    /**
+     * Gets the App.
+     *
+     * @return the app
+     */
     public App getApp() {
         return app;
     }
 
+    /**
+     * Switches the menu.
+     *
+     * @param event the event
+     * @param path  the path
+     */
     public void switchMenu(ActionEvent event, String path) {
 
         try {
-
             FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
 
             if (loader == null) {
@@ -66,14 +91,22 @@ public class SceneController {
         } catch (IOException | MenuNotFoundException error) {
             Alerts.errorAlert(error.getMessage());
         }
-
-
     }
 
+    /**
+     * Gets the company.
+     *
+     * @return the company
+     */
     public Company getCompany() {
         return company;
     }
 
+    /**
+     * Gets the graphical user interface (UI).
+     *
+     * @return the graphical user interface
+     */
     public Runnable getUi() {
         return ui;
     }
--- src/main/java/app/domain/model/Data.java
+++ src/main/java/app/domain/model/Data.java
@@ -19,6 +19,9 @@ public class Data implements Serializable {
     private String selection;
     private int date;
 
+    /**
+     * Constructor.
+     */
     public Data() {
 
         this.historicalDays = null;
@@ -29,44 +32,97 @@ public class Data implements Serializable {
         this.selection = null;
     }
 
+    /**
+     * Sets the ANOVA's confidence level.
+     *
+     * @param confidenceLevelAnova the ANOVA's confidence level
+     * @throws ConfidenceLevelInvalidException
+     */
     public void setConfidenceLevelAnova(double confidenceLevelAnova) throws ConfidenceLevelInvalidException {
         checkConfidenceLevelIC(confidenceLevelAnova);
         this.confidenceLevelAnova = confidenceLevelAnova;
     }
 
+    /**
+     * Sets the confidence level estimated.
+     *
+     * @param confidenceLevelEstimated the confidence level estimated
+     * @throws ConfidenceLevelInvalidException
+     */
     public void setConfidenceLevelEstimated(double confidenceLevelEstimated) throws ConfidenceLevelInvalidException {
         checkConfidenceLevelIC(confidenceLevelEstimated);
         this.confidenceLevelEstimated = confidenceLevelEstimated;
     }
 
+    /**
+     * Sets the confidence level variables.
+     *
+     * @param confidenceLevelVariables the confidence level variables
+     * @throws ConfidenceLevelInvalidException
+     */
     public void setConfidenceLevelVariables(double confidenceLevelVariables) throws ConfidenceLevelInvalidException {
         checkConfidenceLevelIC(confidenceLevelVariables);
         this.confidenceLevelVariables = confidenceLevelVariables;
     }
 
+    /**
+     * Sets the historical days.
+     *
+     * @param historicalDays the historical days
+     * @throws HistoricalDaysEmptyException
+     * @throws HistoricalDaysInvalidException
+     */
     public void setHistoricalDays(String historicalDays) throws HistoricalDaysEmptyException, HistoricalDaysInvalidException {
         checkHistoricaldays(historicalDays);
         this.historicalDays = historicalDays;
     }
 
+    /**
+     * Sets the date intervals.
+     *
+     * @param date the date
+     * @throws DateInvalidException
+     */
     public void setIntervalDates(int date) throws DateInvalidException {
         checkIntervalDates(date);
         this.date = date;
     }
 
+    /**
+     * Sets the dates.
+     *
+     * @param start the start date
+     * @param end   the end date
+     */
     public void setDates(LocalDate start, LocalDate end) {
         this.intervalStartDate = start;
         this.intervalEndDate = end;
     }
 
+    /**
+     * Sets the selected report generation time.
+     *
+     * @param selection the selected report generation time
+     */
     public void setSelection(String selection) {
         this.selection = selection;
     }
 
+    /**
+     * Gets the selected report generation time.
+     *
+     * @return the selected report generation time
+     */
     public String getSelection() {
         return this.selection;
     }
 
+    /**
+     * Checks if the confidence level is valid.
+     *
+     * @param confidenceLevelIC the confidence level
+     * @throws ConfidenceLevelInvalidException
+     */
     private void checkConfidenceLevelIC(double confidenceLevelIC) throws ConfidenceLevelInvalidException {
 
         if (confidenceLevelIC > 100 || confidenceLevelIC < 0) {
@@ -75,6 +131,12 @@ public class Data implements Serializable {
 
     }
 
+    /**
+     * Checks if the date intervals are valid.
+     *
+     * @param date the date
+     * @throws DateInvalidException
+     */
     private void checkIntervalDates(long date) throws DateInvalidException {
 
         if (date < 0) {
@@ -83,6 +145,13 @@ public class Data implements Serializable {
 
     }
 
+    /**
+     * Checks if the historical days are valid.
+     *
+     * @param historicalDays the historical days
+     * @throws HistoricalDaysEmptyException
+     * @throws HistoricalDaysInvalidException
+     */
     private void checkHistoricaldays(String historicalDays) throws HistoricalDaysEmptyException, HistoricalDaysInvalidException {
 
         if (historicalDays.isEmpty() || StringUtils.isBlank(historicalDays)) {
@@ -97,38 +166,73 @@ public class Data implements Serializable {
 
     }
 
+    /**
+     * Gets the number of historical days.
+     *
+     * @return the number of historical days
+     */
     public int getHistoricalDaysInt() {
         return Integer.parseInt(historicalDays);
     }
 
+    /**
+     * Gets the ANOVA's confidence level.
+     *
+     * @return the ANOVA's confidence level
+     */
     public double getConfidenceLevelAnova() {
         return (confidenceLevelAnova / 100);
     }
 
+    /**
+     * Gets the estimated confidence level.
+     *
+     * @return the estimated confidence level
+     */
     public double getConfidenceLevelEstimated() {
         return (confidenceLevelEstimated / 100);
     }
 
+    /**
+     * Gets the confidence levels variables.
+     *
+     * @return the confidence levels variables
+     */
     public double getConfidenceLevelVariables() {
         return (confidenceLevelVariables / 100);
     }
 
+    /**
+     * Gets the historical days.
+     *
+     * @return the historical days
+     */
     public String getHistoricalDays() {
         return historicalDays;
     }
 
+    /**
+     * Gets the end date (interval).
+     * @return the end date
+     */
     public LocalDate getIntervalEndDate() {
         return intervalEndDate;
     }
 
+    /**
+     * Gets the start date (interval).
+     * @return the start date
+     */
     public LocalDate getIntervalStartDate() {
         return intervalStartDate;
     }
 
+    /**
+     * Gets the difference between dates.
+     * @return the difference between the start and end dates
+     */
     public int getDifferenceInDates() {
-
         return (int) ChronoUnit.DAYS.between(this.intervalStartDate, this.intervalEndDate);
-
     }
 
 }
