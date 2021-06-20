d40d8e06b9edb3abf5ad84c0e5e2f37dc6b908be
Eduardo Novo <1200737@isep.ipp.pt>
2021-06-20 13:38:58 +0100

    US17 tests

--- src/test/java/app/domain/model/ImportTestsTest.java
+++ src/test/java/app/domain/model/ImportTestsTest.java
@@ -9,9 +9,39 @@ import org.junit.Test;
 public class ImportTestsTest {
 
     @Test
-    public void CreateValidCategoryTest() {
+    public void readTestFromCSVTest() {
         //Arrange + Act
         String fileName = "csv/tests_BloodMDISCCSV.csv";
+
+        ImportTests aa = new ImportTests();
+        aa.readTestFromCSV(fileName);
+        Assert.assertNotNull(aa.getTestFileList());
+
+    }
+    @Test
+    public void readTestFromCSVTest1() {
+        //Arrange + Act
+        String fileName = "csv/tests_BloodCovidMATCPMDISCCSV.csv";
+
+        ImportTests aa = new ImportTests();
+        aa.readTestFromCSV(fileName);
+        Assert.assertNotNull(aa.getTestFileList());
+
+    }
+    @Test
+    public void readTestFromCSVTest2() {
+        //Arrange + Act
+        String fileName = "csv/tests_CovidMATCPCSV.csv";
+
+        ImportTests aa = new ImportTests();
+        aa.readTestFromCSV(fileName);
+        Assert.assertNotNull(aa.getTestFileList());
+
+    }
+    @Test
+    public void readTestFromBlankCSVTest3() {
+        //Arrange + Act
+        String fileName = "csv/FileInvalid.csv";
 
         ImportTests aa = new ImportTests();
         aa.readTestFromCSV(fileName);
