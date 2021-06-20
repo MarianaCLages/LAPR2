ab0b784318f09c72b55684bc9262b1bb255216e9
Eduardo Novo <1200737@isep.ipp.pt>
2021-06-20 16:27:18 +0100

    US17 tests

--- src/test/java/app/domain/model/ImportTestsTest.java
+++ src/test/java/app/domain/model/ImportTestsTest.java
@@ -5,6 +5,7 @@ package app.domain.model;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.text.ParseException;
 
 
 public class ImportTestsTest {
@@ -72,6 +73,36 @@ public class ImportTestsTest {
 
         String fileName = "csv/Tests3.csv";
         aa.readTestFromCSV(fileName);
+
+    }
+    @Test
+    public void createClientFileTest() throws ParseException {
+        //Arrange + Act
+        ImportTests aa = new ImportTests();
+
+        String fileName = "csv/Tests.csv";
+        aa.readTestFromCSV(fileName);
+        String a = "000000000001;OUQ000003201;001LR;0000000000003201;6000003201;2100003201;05/12/1946;91200003201;Piper GROVES;PiperGROVES2155@gmail.com;128 Street of England 14 Leicester LR 23 OP;Blood;Hemogram;132,4;1;385,2;13,4;Cholesterol;323,8;NA;NA;17/05/2021 8:00;17/05/2021 10:07;18/05/2021 10:32;18/05/2021 11:47";
+        String[] b = a.split(";");
+
+
+        Assert.assertTrue(aa.createClientfromFile(b));
+
+
+
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void createInvalidClientFileTest() throws ParseException {
+        //Arrange + Act
+        ImportTests aa = new ImportTests();
+
+        String a = "000000000001;OUQ000003201;001LR;0000000000003201;6000003201;2100003201;05/12/1946;9120A003201;Piper GROVES;PiperGROVES2155@gmail.com;128 Street of England 14 Leicester LR 23 OP;Blood;Hemogram;132,4;1;385,2;13,4;Cholesterol;323,8;NA;NA;17/05/2021 8:00;17/05/2021 10:07;18/05/2021 10:32;18/05/2021 11:47";
+        String[] b = a.split(";");
+
+
+        Assert.assertFalse(aa.createClientfromFile(b));
+
+
 
     }
 
