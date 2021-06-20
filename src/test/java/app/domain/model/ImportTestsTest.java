c6c2bcf2bff7a1fe494034844cb6651484579410
Eduardo Novo <1200737@isep.ipp.pt>
2021-06-20 14:48:35 +0100

    US17 tests

--- src/test/java/app/domain/model/ImportTestsTest.java
+++ src/test/java/app/domain/model/ImportTestsTest.java
@@ -5,6 +5,11 @@ package app.domain.model;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.nio.charset.CoderMalfunctionError;
+import java.nio.charset.CoderResult;
+import java.nio.charset.MalformedInputException;
+
 
 public class ImportTestsTest {
 
@@ -19,6 +24,27 @@ public class ImportTestsTest {
 
     }
 
+
+    @Test
+    public void readTestFromCSVTest2() {
+        //Arrange + Act
+        String fileName = "csv/Tests1.csv";
+
+        ImportTests aa = new ImportTests();
+        aa.readTestFromCSV(fileName);
+        Assert.assertNotNull(aa.getTestFileList());
+
+    }
+    @Test
+    public void readTestFromCSVTest3() {
+        //Arrange + Act
+        String fileName = "csv/Tests2.csv";
+
+        ImportTests aa = new ImportTests();
+        aa.readTestFromCSV(fileName);
+        Assert.assertNotNull(aa.getTestFileList());
+
+    }
 
 
 }
